298d067ef7a3ec09ed3cfd3de6617b798ef193ed
pablo <tonyp2hoy@gmail.com>
2024-07-20 21:11:26 -0400

    feat: initial setup/creating bd for User only

---
+++ src/main/java/com/pablodev/security/user/User.java
@@ -0,0 +1,25 @@
+package com.pablodev.security.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+}

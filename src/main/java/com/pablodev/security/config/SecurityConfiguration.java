adeae2b299013d49c6aefd8d7b4e63a454195fa8
pablo <tonyp2hoy@gmail.com>
2024-07-21 11:08:11 -0400

    feat: authentication manager - fix securitFilterChain

--- src/main/java/com/pablodev/security/config/SecurityConfiguration.java
+++ src/main/java/com/pablodev/security/config/SecurityConfiguration.java
@@ -25,28 +25,25 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable()
-                .authorizeHttpRequests()
-                .requestMatchers("")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                //sesion managenment -> when we implmented the filter we want a once per request filter
-                // means every request should be authenticated this means that we should not store
-                // the authentication state or the session state -> the session should be stateless
-                // This will help us to ensure that each request should be authenticated ***
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        //sesion managenment -> when we implmented the filter we want a once per request filter
+        // means every request should be authenticated this means that we should not store
+        // the authentication state or the session state -> the session should be stateless
+        // This will help us to ensure that each request should be authenticated ***
 
-                // jwt filter after provider: because I want to execute this filter before the filter
-                // called username password authentication filter, bcs when we implmented the
-                // jwt authentication filter, we check everything and set update the security context holder
-                // and after that we will be calling the username password authentication filter
+        // jwt filter after provider: because I want to execute this filter before the filter
+        // called username password authentication filter, bcs when we implmented the
+        // jwt authentication filter, we check everything and set update the security context holder
+        // and after that we will be calling the username password authentication filter
         return http.build();
     }
 }

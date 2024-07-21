c84ca1edb296d0780bef7deea99d82ca50d476fc
pablo <tonyp2hoy@gmail.com>
2024-07-21 10:55:04 -0400

    feat: Authentication Provider dao - password encoder

--- src/main/java/com/pablodev/security/config/ApplicationConfig.java
+++ src/main/java/com/pablodev/security/config/ApplicationConfig.java
@@ -5,9 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -21,6 +25,20 @@ public class ApplicationConfig {
         //fetch the user from bd
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(()->new UsernameNotFoundException("user not found by detail user service"));
+    }
+    // dao , is the Data Access Object , which is responsible to fetch Userdetails and also encode password
+    //
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
